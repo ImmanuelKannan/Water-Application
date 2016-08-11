27169b82bdd9630a95c704eddadd28be91591c81
Immanuel Kannan <immanueldk92@gmail.com>
2016-08-12 04:40:24 +0800

    Initial Commit

---
+++ Water App/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Water App
+//
+//  Created by Immanuel Kannan on 12/08/2016.
+//  Copyright © 2016 Immanuel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Water App/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Water App
+//
+//  Created by Immanuel Kannan on 12/08/2016.
+//  Copyright © 2016 Immanuel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
