57b5fa4ecb3bbec7701080b2691b878c1cb391d0
Immanuel Kannan <immanueldk92@gmail.com>
2016-08-12 15:00:28 +0800

    Create EntryManager singleton file

---
+++ Water App/EntryManager.h
@@ -0,0 +1,13 @@
+//
+//  EntryManager.h
+//  Water App
+//
+//  Created by Immanuel Kannan on 12/08/2016.
+//  Copyright © 2016 Immanuel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EntryManager : NSObject
+
+@end
---
+++ Water App/EntryManager.m
@@ -0,0 +1,13 @@
+//
+//  EntryManager.m
+//  Water App
+//
+//  Created by Immanuel Kannan on 12/08/2016.
+//  Copyright © 2016 Immanuel. All rights reserved.
+//
+
+#import "EntryManager.h"
+
+@implementation EntryManager
+
+@end
